de8298be59cc8e549bf41a512301687094f71931
praveen45116 <chodepraveenkumar22td0661@svcet.ac.in>
2025-02-19 19:19:15 +0530

    Add files via upload

---
+++ problem.py
@@ -0,0 +1,8 @@
+# reverse stirng with and without using indexing
+
+string = input("Enter the string: ")
+print("The reverse string is: ", string[::-1])  
+# Output: Enter the string: hello
+# The reverse string is: olleh
+# Output: Enter the string: world
+# The reverse string is: dlrow
